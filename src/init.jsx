1928f102b8b666a1fd6fb6f7f1869d47c3e7ffef
Aleksey Zubkov <zubkovf@mail.ru>
2022-03-16 22:30:16 +0700

    Add websockets

--- src/init.jsx
+++ src/init.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-
+import { io } from 'socket.io-client';
 import App from './App.jsx';
 import store from './slices/index.js';
+import SocketContext from './SocketContext.js';
 
-const init = () => (
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+const init = () => {
+  const socket = io();
 
+  socket.on('connect', () => {
+    console.log(socket);
+  });
+
+  return (
+    <SocketContext.Provider value={socket}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </SocketContext.Provider>
+  );
+};
 export default init;
