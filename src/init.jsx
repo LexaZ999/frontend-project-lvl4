06f8c9eeaa55b7e851c2377e086529be8fcd9d91
Aleksey Zubkov <zubkovf@mail.ru>
2022-03-27 11:37:00 +0700

    Connect the Roll bar (pre-setup)

--- src/init.jsx
+++ src/init.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { I18nextProvider } from 'react-i18next';
 import { io } from 'socket.io-client';
+import { Provider as RollBarProvider, ErrorBoundary } from '@rollbar/react';
 import App from './App.jsx';
 import store from './slices/store.js';
 import SocketContext from './SocketContext.js';
@@ -12,14 +13,27 @@ const init = () => {
   const socket = io();
   socketEventHandlers(socket);
 
+  const rollbarConfig = {
+    accessToken: 'dcbb1b987efe4482aea087a97d027b5d',
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+    payload: {
+      environment: 'production',
+    },
+  };
+
   return (
-    <I18nextProvider i18n={i18n}>
-      <SocketContext.Provider value={socket}>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </SocketContext.Provider>
-    </I18nextProvider>
+    <RollBarProvider config={rollbarConfig}>
+      <ErrorBoundary>
+        <I18nextProvider i18n={i18n}>
+          <SocketContext.Provider value={socket}>
+            <Provider store={store}>
+              <App />
+            </Provider>
+          </SocketContext.Provider>
+        </I18nextProvider>
+      </ErrorBoundary>
+    </RollBarProvider>
   );
 };
 export default init;
